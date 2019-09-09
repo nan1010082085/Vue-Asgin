e31ed2d880aa5370f2f9d9ed125d64e4ed9f9a97
杨东楠 <nan1010082085@163.com>
2019-09-10 02:04:18 +0800

    docs git: 修改了忽略文件

---
+++ src/views/maintain/pending-list.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>待处理列表</div>
+</template>
+<script>
+  export default {
+    components: {},
+		mixins: [],
+		name: 'pending-list',
+		data () {
+			return {}
+		},
+		props: {},
+		computed: {},
+		watch: {},
+		created () {},
+		mounted () {},
+		methods: {},
+		filters: {},
+		directives: {},
+		//keep-alive
+		activated () {},
+		deactivated () {},
+		//实例销毁
+		beforeDestroy () {},
+		destroyed () {}
+		//router
+		//beforeRouteLeave ( to, from, next ) {
+		//} 
+  }
+</script>
+<style scoped lang="less">
+
+</style>
